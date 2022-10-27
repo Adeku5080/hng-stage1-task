5ee141e0c61b3492c72032b6a254e683cbfe9874
Ali Adeku <aliadeku.aam@gmail.com>
2022-10-27 22:19:46 +0000

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express= require('express')
+const cors = require('cors')
 
 const PORT = 3000
 
 const app = express()
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.status(200).json({
