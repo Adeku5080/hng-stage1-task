bdf4cd95f8705d9e8b4b44f03be4cbd4ce6f5eda
Ali Adeku <aliadeku.aam@gmail.com>
2022-10-29 09:49:01 +0100

    Change response

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ app.use(cors())
 
 app.get('/',(req,res)=>{
     res.status(200).json({
-        status : "succesful",
-        data:{
+        
             slackUsername : "onimisi",
             backend : true,
             age :25,
             bio : "I am Ali ,a graduate of Electrical Engineering and a full-stack Software Engineer"
         }
-    })
+    )
 })
 
 app.listen(PORT,()=>{
